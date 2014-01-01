1af8a727d1a57f31d7084bc5fc47fc0bdd8d1a26
Eric Saxby <sax@livinginthepast.org>
2014-01-01 11:10:34 -0800

    initial commit

---
+++ lib/smf/solaris.rb
@@ -0,0 +1,7 @@
+require "smf/solaris/version"
+
+module Smf
+  module Solaris
+    # Your code goes here...
+  end
+end
---
+++ lib/smf/solaris/version.rb
@@ -0,0 +1,5 @@
+module Smf
+  module Solaris
+    VERSION = "0.0.1"
+  end
+end
